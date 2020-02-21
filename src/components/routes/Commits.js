8d92e2ff30da8b111e97774ef557cd245ade5d83
Benjamin Dehli <superelg@gmail.com>
2020-02-21 09:17:27 +0100

    Add example component with redux implementation

--- src/components/routes/Commits.js
+++ src/components/routes/Commits.js
@@ -1,17 +1,32 @@
 // Dependencies
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+
+// Components
+import CommitsList from 'components/partials/CommitsList';
 
 // Stylesheets
 import style from 'components/routes/Commits.module.scss';
 
 class Commits extends Component {
 
-    render() {
-        return (<div>
-            <h1>Commits</h1>
-        </div>)
-    }
+  renderCommitsList(commits) {
+    const commitsElements = commits && commits.length
+      ? commits.map(commit => {
+        return (<div key={commit.sha}>{commit.commit.message}</div>)
+      })
+      : null;
+    return commitsElements
+      ? commitsElements
+      : '';
+  }
+
+  render() {
+    return (<div>
+      <h1>Commits</h1>
+      <CommitsList owner='benjamindehli' repo='react-redux-boilerplate' />
+    </div>)
+  }
 }
 
 export default connect(null, null)(Commits);
