fe7310dd60673f2980ff541365485b59808e3f5d
Benjamin Dehli <superelg@gmail.com>
2020-02-21 09:16:56 +0100

    Add example action and reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,7 +3,9 @@ import {combineReducers} from 'redux';
 import {connectRouter} from 'connected-react-router';
 
 // Reducers
+import CommitsReducer from 'reducers/CommitsReducer';
 
 export default(history) => combineReducers({
-  router: connectRouter(history)
+  router: connectRouter(history),
+  commits: CommitsReducer
 });
