af7ef1426abc3fba1516555da1d93a5b9a955dd3
Benjamin Dehli <superelg@gmail.com>
2020-09-04 08:56:21 +0200

    Add template component for container

---
+++ src/components/template/Container.js
@@ -0,0 +1,18 @@
+// Dependencies
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {NavLink} from 'react-router-dom';
+
+// Stylesheets
+import style from 'components/template/Container.module.scss';
+
+class ContentContainer extends Component {
+
+  render() {
+    return (<div className={style.container}>
+      {this.props.children}
+    </div>)
+  }
+}
+
+export default connect(null, null)(ContentContainer);
